8a9f3e620fd5bb14fde08459d1719796f6f1c539
chad hansen <chad.r.hansen@gmail.com>
2021-05-01 03:11:22 +0000

    update 2

--- alta3research-pythoncert01.py
+++ alta3research-pythoncert01.py
@@ -5,14 +5,17 @@ Author: Chad Hansen
 Date:   04/30/2021
 
 """
-import requests
+import requests, sys
+from PIL import Image
+import numpy as np
 
 def main():
 
     idlist=[]
 
     artist=input("Artist?")
-    artistresults=requests.get(f"https://api.artic.edu/api/v1/artworks/search?q={artist}")
+    maxresults=input("Max Results? ")
+    artistresults=requests.get(f"https://api.artic.edu/api/v1/artworks/search?q={artist}&size={maxresults}")
     
     #print(artistresults.json())
     for id in artistresults.json().get("data"):
@@ -20,14 +23,33 @@ def main():
 
     #print(idlist)
 
-    artwork = requests.get(f"https://api.artic.edu/api/v1/artworks/{idlist[0]}")
+    #artwork = requests.get(f"https://api.artic.edu/api/v1/artworks/{idlist[0]}")
     #print(artwork.json())
-    print(artwork.json().get('data').get('title'))
-"""
+    #print(artwork.json().get('data').get('title'))
+    #print(artwork.json().get('data').keys())
     for artid in idlist:
+        #print(type(artid))
+        #print(artid)
         artwork = requests.get(f"https://api.artic.edu/api/v1/artworks/{artid}")
-        #print(artwork.json().get('data'))
-        for art in artwork.json().get('data'):
+        print(f"Artist: {artwork.json()['data']['artist_title']}")
+        print(f"Title: {artwork.json()['data']['title']}")
+        print(f"Date: {artwork.json()['data']['date_display']}")
+        print(f"Dimensions: {artwork.json()['data']['dimensions']}")
+        print(f"Medium: {artwork.json()['data']['medium_display']}")
+        print(f"Art ID: {artid}")
+        print(f"Artist ID: {artwork.json()['data']['artist_id']}")
+        print(f"Artist ID: {artwork.json()['data']['image_id']}")
+        print(f"https://www.artic.edu/iiif/2/{artwork.json()['data']['image_id']}/full/843,/0/default.jpg")
+        print("----------------")
+        
+    print(len(idlist))
+    print(idlist)
+
+
+
+
+
+"""        for art in artwork.json().get('data'):
             print(type(art))
             
             print(f"Artist: {art['artist_title']}")
@@ -36,7 +58,6 @@ def main():
             print(f"Dimensions: {art['dimensions']}")
             print(f"ID: {art['id']}")
             print("*********")
-
     artwork = requests.get('https://api.artic.edu/api/v1/artworks?limit=20')
     #print(artwork.json().get("data"))
     for art in artwork.json().get('data'):
---
+++ img2ascii.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+from PIL import Image
+import numpy as np
+
+#chars = np.asarray(list(' .,:;irsXA253hMHGS#9B&@'))
+
+chars = np.asarray(list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "))
+
+filename="default.jpg.1"
+scaling=.05
+intensity_corr=1.0
+width_corr=7/4
+
+img = Image.open(filename)
+newsize = ( round(img.size[0]*scaling*width_corr), round(img.size[1]*scaling) )
+img = np.sum( np.asarray( img.resize(newsize) ), axis=2)
+img -= img.min()
+img = (1.0 - img/img.max())**intensity_corr*(chars.size-1)
+
+print( "\n".join( ("".join(r) for r in chars[img.astype(int)]) ) )
